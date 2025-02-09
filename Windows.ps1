7baeb3dfca701a0c65f675abe11a280a6f6a227b
indiebreath <indiebreath@proton.me>
2025-02-10 10:32:25 +1100

    Formatting

--- Windows.ps1
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/pwsh
-
-$directory = Read-Host "Enter a directory "
-$filetype = Read-Host "Enter a filetype "
-
-Get-ChildItem -Path $directory* -Filter *$filetype | Select-Object Name,Directory | Format-Table -AutoSize *
